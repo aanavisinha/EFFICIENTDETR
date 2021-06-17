ecbfb9b827aa4e72c3046dc3ffe3cb995d2d418a
Aanavi <73659591+aanavisinha@users.noreply.github.com>
2021-06-17 15:03:28 +0200

    Update coco.py

--- datasets/coco.py
+++ datasets/coco.py
@@ -4,12 +4,6 @@ COCO dataset which returns image_id for evaluation.
 
 Mostly copy-paste from https://github.com/pytorch/vision/blob/13b35ff/references/detection/coco_utils.py
 """
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-"""
-COCO dataset which returns image_id for evaluation.
-
-Mostly copy-paste from https://github.com/pytorch/vision/blob/13b35ff/references/detection/coco_utils.py
-"""
 from pathlib import Path
 
 import torch
@@ -149,8 +143,7 @@ def make_coco_transforms(image_set):
 
     raise ValueError(f'unknown {image_set}')
     
-    
-    
+
 
 def make_voc_transforms(image_set, args):
 
@@ -212,16 +205,15 @@ def make_voc_transforms(image_set, args):
         ])
 
 
-
 def build(image_set, args):
     root = Path(args.coco_path)
     assert root.exists(), f'provided COCO path {root} does not exist'
     mode = 'instances'
     PATHS = {
-        "train": (root / "images", root / "annotations" / f'{mode}_train2017.json'),
-        "val": (root / "images", root / "annotations" / f'{mode}_val2017.json'),
+        "train": (root / "images", root / "annotations" / f'{mode}_trainval.json'),
+        "val": (root / "images/JPEGImages", root / "annotations" / f'{mode}_test.json'),
     }
 
     img_folder, ann_file = PATHS[image_set]
-    dataset = CocoDetection(img_folder, ann_file, transforms=make_voc_transforms(image_set), return_masks=args.masks)
+    dataset = CocoDetection(img_folder, ann_file, transforms=make_voc_transforms(image_set, args), return_masks=args.masks)
     return dataset
